abaa87d2c6983e87bc9e83a090d4e589af9e8363
SameerSundaray <sameersundaray2014@gmail.com>
2024-04-09 11:22:37 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ProductsScreen from './components/ProductsScreen';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My E-commerce App</h1>
       </header>
+      <main>
+        <ProductsScreen />
+      </main>
     </div>
   );
 }
 
 export default App;
+
